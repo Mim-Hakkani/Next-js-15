60d8566d939f3232fd1a6dc186a7b2060f6488ee
Mim-Hakkani <mimhakkani4g@gmail.com>
2024-11-27 15:40:11 +0600

    catch all route

---
+++ app/basic-routing/products/[...slug]/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CatchAllRoutes = () => {
+    return (
+        <div>
+            This is catch all route pagess....
+        </div>
+    );
+};
+
+export default CatchAllRoutes;

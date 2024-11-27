ad0db7f62542940dcfc19348f2000c3f2a3de4dc
Mim-Hakkani <mimhakkani4g@gmail.com>
2024-11-27 14:58:33 +0600

    next js basic routiong is done

---
+++ app/basic-routing/news/article/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ArticleIndexPage = () => {
+    return (
+        <div>
+            THis is the article index page...
+        </div>
+    );
+};
+
+export default ArticleIndexPage;
---
+++ app/basic-routing/news/blogs/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogPage = () => {
+    return (
+        <div>
+            <h1>This is the Blog page</h1>
+        </div>
+    );
+};
+
+export default BlogPage;
